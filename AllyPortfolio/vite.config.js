f01087224f24ff65988983cbebfd235b48c58fcc
Allyssaur <owllyssa@gmail.com>
2026-02-26 13:16:49 -0600

    is this the router fix I am looking for

--- AllyPortfolio/vite.config.js
+++ AllyPortfolio/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/allycatportfolio/",
+  base: "https://allyssaur.github.io/allycatportfolio/",
   optimizeDeps: {
     include: ["react", "react-dom" ],
   },
