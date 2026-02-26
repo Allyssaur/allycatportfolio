79224446c2cb638484f55cc8a5abae9449b223f6
Allyssaur <owllyssa@gmail.com>
2026-02-26 13:11:17 -0600

    Artist Guide Pages and Links Fixed

--- AllyPortfolio/src/components/OtherPages/PV/ArtistGuide.jsx
+++ AllyPortfolio/src/components/OtherPages/PV/ArtistGuide.jsx
@@ -3,24 +3,23 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import './ArtistGuide.scss';
-import P1 from './Images/ArtistGuidePages.png'
 
 gsap.registerPlugin(ScrollTrigger);
 
 const pageData = [
-  {front: {P1}, back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages2.png?raw=true", isCover: true,},
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages3.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages4.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages5.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages6.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages7.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages8.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages9.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages10.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages11.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages12.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages13.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages14.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages15.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages16.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages17.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages18.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages19.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages20.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages21.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages22.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages23.png?raw=true", back: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages24.png?raw=true" },
-  { front: "https://github.com/Allyssaur/PortfolioNodeFix/blob/main/GDPortfolio/src/assets/ArtistGuidePages25.png?raw=true", back: "#" },
+  {front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages2.png?raw=true", isCover: true,},
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages3.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages4.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages5.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages6.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages7.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages8.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages9.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages10.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages11.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages12.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages13.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages14.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages15.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages16.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages17.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages18.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages19.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages20.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages21.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages22.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages23.png?raw=true", back: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages24.png?raw=true" },
+  { front: "https://github.com/Allyssaur/allycatportfolio/blob/main/AllyPortfolio/src/components/OtherPages/PV/Images/ArtistGuidePages25.png?raw=true", back: "null" },
 ]
 
 export default function Guide() {
